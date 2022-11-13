8082a517c6a3dc05ca22e82c90a1409c1ef4c2a8
Hein02 <117172810+Hein02@users.noreply.github.com>
2022-11-13 05:29:07 -0800

    Add random color for drawing

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ class Model {
     this.sqWidth = 0;
     this.totalSq = 0;
     this.sqPerSide = 0;
+    this.sqColor = 'blue';
   }
 
   setup(sqPerSide = 16) {
     this.sqPerSide = sqPerSide;
     this.sqWidth = this.canvasWidth / this.sqPerSide;
     this.totalSq = Math.pow(this.sqPerSide, 2);
+  }
+
+  setSqColor(color) {
+    this.sqColor = color;
   }
 }
 
@@ -19,10 +24,11 @@ class View {
     this.canvas = document.querySelector('.js-canvas');
     this.sqPerSideInput = document.querySelector('.js-sq-per-side');
     this.createBtn = document.querySelector('.js-create-btn');
+    this.randomBtn = document.querySelector('.js-random-btn');
   }
 
-  bindEvent(el, handler, type) {
-    el.addEventListener(type, handler);
+  bindEvent(el, handler, type, ...options) {
+    el.addEventListener(type, handler, ...options);
   }
 
   getInput(inputEl) {
@@ -43,19 +49,39 @@ class View {
     parent.append(child);
   }
 
-  renderCanvas(totalSq) {
-    while (this.canvas.firstChild) {
-      this.canvas.removeChild(this.canvas.firstChild);
+  removeChildren(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
     }
+  }
+
+  createSq(totalSq, style) {
     let frag = document.createDocumentFragment();
     for (let i = 0; i < totalSq; i += 1) {
       let el = this.createEl('div', 'square');
-      el.addEventListener('mouseover', (e) => {
-        e.target.classList.add('draw');
+      this.bindEvent(el, this.draw(style), 'mouseover', {
+        once: true,
       });
       frag.append(el);
     }
-    this.renderEl(this.canvas, frag);
+    return frag;
+  }
+
+  draw = (style) => (e) => {
+    if (style === 'random') {
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+      e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    } else {
+      e.target.classList.add('draw');
+    }
+  };
+
+  renderCanvas(totalSq, style) {
+    const squares = this.createSq(totalSq, style);
+    this.removeChildren(this.canvas);
+    this.renderEl(this.canvas, squares);
   }
 
   setCustomProperty(name, value) {
@@ -78,7 +104,8 @@ class Controller {
       this.handleSqPerSideChange,
       'change'
     );
-    this.view.bindEvent(this.view.createBtn, this.handleCreate, 'click');
+    this.view.bindEvent(this.view.createBtn, this.handleCreateCanvas, 'click');
+    this.view.bindEvent(this.view.randomBtn, this.handleRandomColor, 'click');
     this.view.renderCanvas(this.model.totalSq);
     this.bindCanvas();
   }
@@ -89,17 +116,24 @@ class Controller {
     this.model.setup(sqPerSide);
   };
 
-  handleCreate = () => {
+  handleCreateCanvas = () => {
     this.view.clearInput(this.view.sqPerSideInput);
     this.bindCanvas();
     this.view.renderCanvas(this.model.totalSq);
   };
 
+  handleRandomColor = () => {
+    this.model.setSqColor('random');
+    this.view.renderCanvas(this.model.totalSq, this.model.sqColor);
+  };
+
   bindCanvas() {
     const canvasWidth = `${this.model.canvasWidth}px`;
     const sqWidth = `${this.model.sqWidth}px`;
+    const sqColor = this.model.sqColor;
     this.view.setCustomProperty('--canvas-width', canvasWidth);
     this.view.setCustomProperty('--square-width', sqWidth);
+    this.view.setCustomProperty('--square-color', sqColor);
   }
 }
 
